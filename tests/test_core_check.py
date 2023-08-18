db1d7097ae85fca784529a4178519c11545a2c77
ayasyrev <a.yasyrev@gmail.com>
2023-08-18 12:25:06 +0300

    tests by files

--- tests/test_core_check.py
+++ tests/test_core_check.py
@@ -1,4 +1,6 @@
-from cli_result.core import Cfg, check_examples
+import pytest
+
+from cli_result.core import Cfg, check_examples, get_examples, run_check_example
 
 
 def test_check_examples():
@@ -7,19 +9,48 @@ def test_check_examples():
     results = check_examples()
     assert results is None
 
-    # default config
-    results = check_examples()
+    # extra
+    examples_path = "examples/examples_extra"
+    results = check_examples(cfg=Cfg(examples_path=examples_path))
     assert results is None
 
-    # extra
-    cfg = Cfg(examples_path="examples/examples_extra")
-    results = check_examples(cfg=cfg)
+
+cfg = Cfg(examples_path="examples/")
+examples = get_examples(cfg=cfg)
+
+
+@pytest.mark.parametrize("example_name, file_list", examples)
+def test_run_check_example(example_name, file_list):
+    """test run_check_example"""
+    results = run_check_example(example_name, file_list, cfg=cfg)
     assert results is None
+
+
+cfg = Cfg(examples_path="examples/examples_extra")
+examples = get_examples(cfg=cfg)
+
+
+@pytest.mark.parametrize("example_name, file_list", examples)
+def test_run_check_example_extra(example_name, file_list):
+    """test run_check_example"""
+    results = run_check_example(example_name, file_list, cfg=cfg)
+    assert results is None
+
+
+cfg = Cfg(examples_path="tests/examples/examples_errors")
+examples = get_examples(cfg=cfg)
+
+
+@pytest.mark.parametrize("example_name, file_list", examples)
+def test_run_check_examples_errors(example_name, file_list):
+    """test check_examples with errors"""
+    # errors
+    results = run_check_example(example_name, file_list, cfg=cfg)
+    assert results
 
 
 def test_check_examples_errors():
     """test check_examples with errors"""
     # errors
-    cfg = Cfg(examples_path="tests/examples/examples_errors")
     results = check_examples(cfg=cfg)
     assert results
